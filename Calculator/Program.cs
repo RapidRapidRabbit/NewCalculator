fc3a8a970b7eb1fa80157a41feedab9a3ca52add
RapidRapidRabbit <awestruck31@mail.ru>
2022-06-26 04:51:23 +0300

    refactoring

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,52 +5,42 @@ using System.Linq;
 
 namespace NewCalc
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static readonly char[] ValidOperations = { '+', '-', '*', '/' };
+
+        public static void Main(string[] args)
         {
             while (true)
             {
                 try
                 {
-                    BigInt firstValue = new BigInt(0);
-                    BigInt secondValue = new BigInt(0);
-                    BigInt result = new BigInt(0);
+                    var firstValue = new BigInt(0);
+                    var secondValue = new BigInt(0);
+                    var result = new BigInt(0);
                     char operation;
-                    char[] validOperations = { '+', '-', '*', '/' };
-
-
-                    // cледующее просто показывает работу реализации интерфейса IComparable
-
-                    /*BigInt[] numbersForSorting = { new BigInt(100), new BigInt(15), new BigInt(45345), new BigInt("44"), new BigInt(500), new BigInt(-500) };
-                    Array.Sort(numbersForSorting);
-                    foreach (var number in numbersForSorting)
-                    {
-                        Console.WriteLine(number);
-                    }*/
 
                     Console.WriteLine("Введите первое число");
+
                     firstValue = new BigInt(GetValue());
-
-
 
                     while (true)
                     {
-                        Console.WriteLine("Желаемая математическая операция (+ - * /)");
+                        Console.WriteLine("Желаемая арифметическая операция (+ - * /)");
 
-                        if (char.TryParse(Console.ReadLine(), out char oper) && validOperations.Contains(oper))
+                        if (char.TryParse(Console.ReadLine(), out char oper) && ValidOperations.Contains(oper))
                         {
                             operation = oper;
                             break;
                         }
-                        else
-                            Console.WriteLine("Неверный оператор, попробуйте еще раз");
+
+                        Console.WriteLine("Неверный оператор, попробуйте еще раз");
                     }
 
 
                     Console.WriteLine("Введите второе число");
-                    secondValue = new BigInt(GetValue());
 
+                    secondValue = new BigInt(GetValue());
 
                     switch (operation)
                     {
@@ -77,34 +67,26 @@ namespace NewCalc
                 {
                     Console.WriteLine("Что-то пошло не так, попробуйте еще раз");
                 }
-                
             }
+        }
 
-            string GetValue()
+        private static bool CheckValue(string value) => value.All(char.IsNumber);
+
+        private static string GetValue()
+        {
+            string value;
+
+            while (true)
             {
-                string value = string.Empty;
+                value = Console.ReadLine();
 
-                while (true)
-                {
-                    value = Console.ReadLine();
-                    if (CheckValue(value))
-                        break;
-                    else
-                        Console.WriteLine("Ошибка, некорректные данные, попробуйте еще раз");
-                }
+                if (CheckValue(value))
+                    break;
 
-                return value;
+                Console.WriteLine("Ошибка, некорректные данные, попробуйте еще раз");
             }
-            
-            bool CheckValue(string value)
-            {
-                foreach (var symbol in value)
-                {
-                    if (!char.IsNumber(symbol))
-                        return false;
-                }
-                return true;
-            }
+
+            return value;
         }
 
     }
